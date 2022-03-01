820c80bf3a8d597fef8cc111a54f3655f2363b4a
Kuncans <akafauna@gmail.com>
2022-03-01 10:35:15 +0000

    Initial Commit

---
+++ Exams-App/Exams_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exams_AppApp.swift
+//  Exams-App
+//
+//  Created by Duncan Kent on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Exams_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
