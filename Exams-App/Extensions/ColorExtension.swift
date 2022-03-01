4ef92a5be6f16e6aae3d9a8b13d51b00f23202bc
Kuncans <akafauna@gmail.com>
2022-03-01 11:29:39 +0000

    Initial commit, add colour extension

---
+++ Exams-App/Extensions/ColorExtension.swift
@@ -0,0 +1,22 @@
+//
+//  ColorExtension.swift
+//  Exams-App
+//
+//  Created by Duncan Kent on 01/03/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    
+    
+}
